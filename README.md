b272de142d94fdec2ee29fd98da85a0651a99a82
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-09-14 10:10:13 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# Next_Todos
-Todo application utilizing Next.js 
+
+## Getting Started
+
+This application is a basic todo list generator that utilizes server side rendering to display components. The app utilizes a local prisma database which will need to be initialized prior to running the application.
+
+First, 
+
+```npm i prisma --save-dev```
+
+```npx prisma init --datasource-provider sqlite```
+
+create database schema in prisma file ... 
+
+Create a local database by creating an .env file
+example:
+
+'''
+DATABASE_URL="file:./dev.db"
+'''
+
+do a migration into our dev environemnt:
+
+```npx prisma migrate dev --name init```
+
+Then, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+
+This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+
---
+++ src/app/page.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Link from "next/link"
+import { toEditorSettings } from 'typescript'
+import { prisma } from './db'
+import { TodoItem } from './components/TodoItem'
+
+function getTodos(){
+  return prisma.todo.findMany() 
+}
+
+async function toggleTodo(id: string, complete: boolean ){
+  "use server"
+  console.log(id, complete)
+
+  await prisma.todo.update({where: { id }, data: { complete }})
+}
+
+async function Home() {
+
+// all this code is going to run on server which gets sent to client
+// await prisma.todo.create({data: {title: "test", complete: false}})
+
+  const todos = await getTodos();
+  return (
+    <>
+    <header className = "flex justify-between items-center mb-4">
+      <h1 className = "text-2xl">Todos</h1>
+      <Link 
+      className='border border-slate-300 text-slate-300 px-2 py-1 rounded
+      hover:bg-slate-700 focus-within:bg-slate-700 outline none'
+      href = "/new">New</Link>
+    </header>
+    <ul className = "pl-4">
+      {todos.map(todo => (
+       <TodoItem key = {todo.id} {...todo} toggleTodo={toggleTodo}/>
+      ))}
+    </ul>
+    </>
+  )
+}
+
+export default Home
